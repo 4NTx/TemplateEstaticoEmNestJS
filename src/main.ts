bf7a3ea96177e06fb9074e24d697afa00e419c1e
4NTx <arturrrick@gmail.com>
2024-04-22 20:04:00 -0300

    Adicionado suporte para servir arquivos estáticos

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import fastifyHelmet from '@fastify/helmet';
-import fastifyCors from '@fastify/cors';  
+import fastifyCors from '@fastify/cors';
+import fastifyStatic from '@fastify/static';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -10,14 +12,19 @@ async function bootstrap() {
     new FastifyAdapter()
   );
 
+  app.register(fastifyStatic, {
+    root: join(__dirname, '..', 'public', 'out'),
+    prefix: '/',
+  });
+
   await app.register(fastifyHelmet, {
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
         styleSrc: ["'self'", 'https:', 'unsafe-inline'],
         fontSrc: ["'self'", 'https:', 'data:'],
-        imgSrc: ['data:', 'https:'],
-        scriptSrc: ["'self'", "https://caminhoDéScriptsAquiç.com"],
+        imgSrc: ['data:', 'https:', 'http://127.0.0.1:3000'],
+        scriptSrc: ["'self'", "https://caminhoDeScriptsAqui.com"],
       }
     }
   });
